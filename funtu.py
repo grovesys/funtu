22c5c794b20b18ac52c72a54ee3487a124ab8589
grovesys <grovesys@users.noreply.github.com>
2015-06-27 21:05:09 +0100

    Update funtu.py

--- funtu.py
+++ funtu.py
@@ -6,8 +6,15 @@
 # Any use of this code is done so at your own risk!  #
 ######################################################
 
-import urllib as url # urllib is required to allow us to use .urlopen
-targetUrl = "http://www.malware-traffic-analysis.net/blog-entries.html" # Destination to scrape
+from lxml import html
+import requests
+targetUrl = "http://www.malware-traffic-analysis.net/blog-entries.html" #Destination location
 
-web_data = url.urlopen(targetUrl)
-print web_data.read()
+page = requests.get(targetUrl) #Request the URL specified in the targetURL
+fullPage = html.fromstring(page.text) #Return the full source of the page
+
+date = fullPage.xpath('//a[@class="list_header"]/text()') #This will create the list of all the entries in list_header from the html returned
+subject = fullPage.xpath('//a[@class="main_menu"]/text()') ## This will create the list of all the entries in main_menu from the html returned
+
+print "Date: ", date[7] # Return number 8 in the list
+print "Subject: ", subject[7] # Return number 8 in the list
